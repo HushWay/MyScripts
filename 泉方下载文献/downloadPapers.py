0cb73e87e20c4275a519658c06c65989cf0d79fd
liuwei <pseudoway@163.com>
2022-10-28 16:50:28 +0800

    完成 泉方 文献下载脚本

--- 泉方下载文献/downloadPapers.py
+++ 泉方下载文献/downloadPapers.py
@@ -1,4 +1,7 @@
 from matplotlib.pyplot import close
+import os
+from os import listdir
+from os.path import isfile, join
 from selenium import webdriver
 from time import sleep
 from selenium import webdriver
@@ -30,41 +33,48 @@ def tiny_file_rename(newname, folder_of_download, time_to_wait=60):
 
 def fetchPDF(PMID, file_name):
     driver.get("https://pm.yuntsg.com/searchList.html")
-    search_box = WebDriverWait(driver, 20).until(
-        EC.element_to_be_clickable((By.ID, "keyWord")))
-    search_box.clear()
-    search_box.send_keys(PMID)
+    sleep(2)
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "keyWord"))).clear()
+    sleep(1)
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.ID, "keyWord"))).send_keys(PMID)
+    sleep(1)
     driver.find_element_by_class_name("searchBtn").click()
-
+    sleep(1)
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//ul[@class='tabOperation']//li[contains(text(),'申请全文')]"))).click()
+    sleep(1)
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//div[@class='listTab']//a[1]"))).click()
     sleep(1)
     driver.switch_to.window(driver.window_handles[1])
+    sleep(1)
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'保存PDF')]"))).click()
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
+    sleep(10) # 每个文件给 10s 下载时间
     tiny_file_rename(file_name, download_path)
 
 
 #######
 # ' 下载路径配置
 #######
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# current_dir = "D:\\MyPythonScripts\\泉方下载文献"
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = "D:\\MyPythonScripts\\泉方下载文献"
 download_path = os.path.join(current_dir, "papers")
+
 
 #######
 # ' 配置浏览器，后台静默运行
 #######
 option = webdriver.ChromeOptions()
-option.add_argument('--headless')
+# option.add_argument('--headless')
 option.add_argument('--no-sandbox')
 option.add_argument('--disable-dev-sh-usage')
 option.add_argument('--start-maximized')
-options.add_experimental_option('prefs', {
+option.add_experimental_option('prefs', {
     # Change default directory for downloads
     "download.default_directory": download_path,
     "download.prompt_for_download": False,  # To auto download the file
@@ -72,33 +82,43 @@ options.add_experimental_option('prefs', {
     # It will not show PDF directly in chrome
     "plugins.always_open_pdf_externally": True
 })
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=option)
 
 #######
 # ' 登陆
 #######
 driver.get("https://user.tsgyun.com/user/login?isOffWebsite=true")
+# WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
+#     (By.ID, "username"))).send_keys("18845181393")
+# driver.find_element_by_id("password1").send_keys("liuwei78")
 WebDriverWait(driver, 20).until(EC.visibility_of_element_located(
-    (By.ID, "username"))).send_keys("18845181393")
+    (By.ID, "username"))).send_keys("18846116416")
 driver.find_element_by_id("password1").send_keys("liuwei78")
-captcha = input("Enter CAPTCHA and Press ENTER\n")
-driver.find_element_by_id("code1").send_keys(captcha)
-driver.find_element_by_xpath(
-    "(//button[@class='layui-btn layui-btn-fluid'][contains(text(),'登 录')])[1]").click()
+# captcha = input("Enter CAPTCHA and Press ENTER\n")
+# print("验证码是：" + captcha)
+# driver.find_element_by_id("code1").send_keys(captcha)
+# sleep(1)
+# driver.find_element_by_xpath(
+#     "(//button[@class='layui-btn layui-btn-fluid'][contains(text(),'登 录')])[1]").click()
 
 #######
 # ' 下载文献
 #######
+# 已下载文献
+downloaded_files = [f for f in listdir(download_path) if isfile(join(download_path, f))]
+download_ids = [os.path.splitext(f)[0] for f in downloaded_files]
 paper_fn = os.path.join(current_dir, "papers.txt")
 paper = open(paper_fn)
 Lines = paper.readlines()
+
 for line in Lines:
-    line = Lines[1]
     source = line.strip().split("\t")
     PMID = source[0]
     file_name = source[1]
     print(source)
+    if file_name in download_ids:
+        continue
     try:
-        fetchPDF(PMID=PMID, file_name=file_name)
+        fetchPDF(PMID=PMID, file_name=file_name+".pdf")
     except:
         pass
