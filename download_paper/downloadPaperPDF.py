9fc46a0014a7ae863fe2e04ba876bf76effac0bd
liuwei <pseudoway@163.com>
2023-08-10 15:41:28 +0800

    增加log文件记录任务

--- download_paper/downloadPaperPDF.py
+++ download_paper/downloadPaperPDF.py
@@ -1,6 +1,3 @@
-# TODO: 使用说明
-# ...
-
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
@@ -23,6 +20,7 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 download_path = os.path.join(current_dir, "papers")
 driver = configChromeDriver(download_path=download_path)
 
+
 #### 登陆泉方账号 ####
 def inputByID(driver, id, value):
     ele = driver.find_element(By.ID, id)
@@ -36,8 +34,10 @@ def logQuanFang(username = "2628201733@qq.com", password = "liuwei78"):
 
 logQuanFang()
 # TODO: 手动输入验证码
-print("请在1分钟内输入账号、密码、验证并点击确定...")
-sleep(60)
+print("请输入账号、密码、验证并点击登陆，完成后按下Enter键继续...")
+input()
+print("开始执行下载任务...")
+
 
 #### 检索文献 ####
 def searchPMID(driver, pmid):
@@ -58,10 +58,10 @@ def downloadPDF(driver):
     switchWindow(driver, 1)
     try:
         driver.execute_script("downloadPdf()")
-        print("下载成功")
-    except:
-        print("下载失败")
-    driver.close()
+    except Exception as e:
+        raise
+    finally:
+        driver.close()
 
 def downloadByPMID(driver, pmid, wait_sec = 5):
     sleep(wait_sec)
@@ -71,9 +71,12 @@ def downloadByPMID(driver, pmid, wait_sec = 5):
     sleep(wait_sec*2)
     downloadPDF(driver)
 
+
 #### 读取PMID列表，进行文献下载 ####
 def processLine(fn,f,*args, retry_count = 1):
-    file = open(fn)
+    file = open(fn, "r")
+    log_fn = fn + ".log"
+    log_file = open(log_fn, "w")
     lines = file.readlines()
 
     for item in lines:
@@ -83,6 +86,7 @@ def processLine(fn,f,*args, retry_count = 1):
         while retry_indx <= retry_count:
             try:
                 f(*args, pmid)
+                log_line = pmid + "\tSuccess"
                 break # 成功的话跳出
             except Exception as e:
                 print(f"Error processing {pmid}:{e}")
@@ -91,7 +95,10 @@ def processLine(fn,f,*args, retry_count = 1):
                     retry_indx += 1
                 else:
                     print("重试失败. 跳过。")
+                    log_line = pmid + "\tError"
                     break
+        log_file.write(log_line + "\n")
+    print("任务结束，详情查看：{log_fn}")
 
 paper_fn = os.path.join(current_dir, "papers.txt")
 processLine(paper_fn, downloadByPMID, driver)
