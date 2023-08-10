0c00ae10a29ca0e66b96324c0bdf4e3f61801b45
liuwei <pseudoway@163.com>
2023-08-10 08:55:32 +0800

    重写文献下载函数，模块化

--- 泉方下载文献/downloadPaperPDF.py
+++ 泉方下载文献/downloadPaperPDF.py
@@ -1,10 +1,11 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import os
 
 #### 配置 webdriver ####
 def configChromeDriver(download_path):
-
     option = webdriver.ChromeOptions()
     option.add_experimental_option('prefs', {
         "download.default_directory": download_path,
@@ -14,7 +15,9 @@ def configChromeDriver(download_path):
     })
     driver = webdriver.Chrome(options=option)
     return driver
-download_path = "D:/OneDrive - hrbmu.edu.cn/workspace/MyScripts/泉方下载文献/papers"
+# current_dir = os.path.dirname(os.path.abspath(__file__))
+current_dir = "D:\\OneDrive - hrbmu.edu.cn\\workspace\\MyScripts\\泉方下载文献"
+download_path = os.path.join(current_dir, "papers")
 driver = configChromeDriver(download_path=download_path)
 
 #### 登陆泉方账号 ####
@@ -23,17 +26,18 @@ def inputByID(driver, id, value):
     ele.clear()
     ele.send_keys(value)
 
-def logQuanFang(username = "18846116416", password = "liuwei78"):
+def logQuanFang(username = "2628201733@qq.com", password = "liuwei78"):
     driver.get("https://user.tsgyun.com/user/login?isOffWebsite=true")
     inputByID(driver, "username", username)
     inputByID(driver, "password1", password)
 
 logQuanFang()
-# TODO: 输入验证码
+# TODO: 手动输入验证码
 
 #### 检索文献 ####
 def searchPMID(driver, pmid):
     driver.get("https://pm.yuntsg.com/searchList.html")
+    sleep(5)
     inputByID(driver, "keyWord", pmid)
     driver.find_element(By.CLASS_NAME, "searchBtn").click()
 
@@ -43,10 +47,44 @@ def switchWindow(driver, index):
 def applyFirstPDF(driver):
     driver.find_element(By.XPATH, "//ul[@class='tabOperation']//li[contains(text(),'申请全文')]").click()
     driver.find_element(By.XPATH, "//div[@class='listTab']//a[1]").click()
+
+def downloadPDF(driver):
+    switchWindow(driver, 1)
+    driver.execute_script("downloadPdf()")
+    driver.close()
     switchWindow(driver, 0)
 
-def downloadPaper(driver, pmid):
+def downloadByPMID(driver, pmid, wait_sec = 5):
+    sleep(wait_sec)
+    searchPMID(driver, pmid)
+    sleep(wait_sec)
+    applyFirstPDF(driver)
+    sleep(wait_sec*2)
+    downloadPDF(driver)
 
+#### 读取PMID列表，进行文献下载 ####
+def processLine(fn,f,*args, retry_count = 1):
+    file = open(fn)
+    lines = file.readlines()
 
-pmid = 36289336
-searchPMID(driver, pmid)
+    for item in lines:
+        pmid = item.strip()
+        print(pmid)
+        retry_indx = 0
+        while retry_indx <= retry_count:
+            try:
+                f(*args, pmid)
+                print("成功")
+                break # 成功的话跳出
+            except Exception as e:
+                print(f"Error processing {pmid}:{e}")
+                if retry_indx <= retry_indx:
+                    print("重试中...")
+                    retry_indx += 1
+                else:
+                    print("重试失败. 跳过。")
+                    break
+
+            
+paper_fn = os.path.join(current_dir, "papers.txt")
+processLine(paper_fn, downloadByPMID, driver)
